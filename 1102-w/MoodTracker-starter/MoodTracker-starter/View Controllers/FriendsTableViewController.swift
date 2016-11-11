c7a77975e64a8d311301d11c04b7f1eb590d14e4
Tassos Lambrou <lambrou@gmail.com>
2016-11-11 12:51:03 -0800

    Fixed repo

--- 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
+++ 1102-w/MoodTracker-starter/MoodTracker-starter/View Controllers/FriendsTableViewController.swift
@@ -10,12 +10,79 @@ import UIKit
 
 class FriendsTableViewController: UITableViewController {
   
+  static let happyString = "Oh happy day..."
+  static let badString = "Get off my lawn!!!"
+  static let mediumString = "Well, I don't really care."
+  
+  static var friendArray = [
+    Friend(name: "John", mood: .happy),
+    Friend(name: "Jack", mood: .happy),
+    Friend(name: "Sarah", mood: .happy),
+    Friend(name: "Jenny", mood: .happy)
+  ]
+  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return FriendsTableViewController.friendArray.count
   }
-
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    print(indexPath.row)
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "FriendTableViewCell") as! FriendTableViewCell
+    
+    let friend = FriendsTableViewController.friendArray[indexPath.row]
+    cell.nameLabel.text = friend.name
+    
+    cell.friendTableViewController = self
+    
+    cell.moodButton.tag = indexPath.row
+    switch FriendsTableViewController.friendArray[cell.moodButton.tag].mood{
+    case .angry:
+        cell.moodDescriptionLabel.text = FriendsTableViewController.badString
+    case .medium:
+        cell.moodDescriptionLabel.text = FriendsTableViewController.mediumString
+    case .happy:
+        cell.moodDescriptionLabel.text = FriendsTableViewController.happyString
+    }
+    cell.moodButton.titleLabel?.text = friend.mood.rawValue
+    cell.moodButton.setTitle(friend.mood.rawValue, for: .normal)
+    
+    
+    return cell
+  }
+    
+    func updateMood(friend: Friend){
+        
+        
+        
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "FriendTableViewCell") as! FriendTableViewCell
+//        switch friendArray[index].mood{
+//        case .angry:
+//            friendArray[index].mood = .medium
+//            cell.moodDescriptionLabel.text = FriendsTableViewController.mediumString
+//        case .medium:
+//            cell.moodDescriptionLabel.text = FriendsTableViewController.mediumString
+//        case .happy:
+//            cell.moodDescriptionLabel.text = FriendsTableViewController.happyString
+//        }
+        
+    }
+    
+    private func nextMood(mood: Mood) -> Mood {
+    switch mood {
+    case .happy:
+        return .medium
+    case .medium:
+        return .angry
+    case .angry:
+        return .happy
+    }
+    
+    }
+    
+    
 }
